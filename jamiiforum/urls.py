f5f9884a6fb0ce4038dff177a4806e8357835210
Dennis Aziaya <aziayadennis@gmail.com>
2020-04-06 13:17:06 +0300

    user profile edit form added

--- jamiiforum/urls.py
+++ jamiiforum/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', views.home, name='home'),
                   path('account/', include('account.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
